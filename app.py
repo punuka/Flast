14e11afc36d4a612988f47a6aa6129430c94ae76
punuka <esunyen@gmail.com>
2025-07-14 19:51:20 +0300

    initial commit

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
-from flask import Flask, request, render_template
+from flask import Flask,render_template,request
 import joblib
-import numpy as np
-app = Flask(__name__)
+app=Flask(__name__)
 model = joblib.load('titanic_model.pkl')
-@app.route('/')
+@app.route("/")
 def home():
     return render_template('index.html')
-@app.route('/predict', methods=['POST'])
+@app.route("/predict", methods=["POST"])
 def predict():
-    features = [float(x) for x in request.form.values()]
-    prediction = model.predict([features])[0]
-    return render_template('index.html', prediction_text='Survived' if prediction == 1 else
-'Not Survived')
+    sex = 1 if request.form['sex'] == 'female' else 0
+    fare = float(request.form['fare'])
+    prediction = model.predict([[sex,fare]])[0]
+    result = "Survived" if prediction == 1 else 'Did not Survive'
+    return render_template('index.html', prediction = result)
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug = True)
--- train_model.py
+++ train_model.py
@@ -1,21 +1,16 @@
 import pandas as pd
+import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-import numpy as np
 import joblib
-# Load dataset
-df =pd.read_csv('https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv')
-# Drop missing and unnecessary columns
-df.drop(['Name', 'Cabin', 'Ticket', 'Embarked'], axis=1, inplace=True)
-df.dropna(inplace=True)
-# Convert categorical column
-df['Sex'] = df['Sex'].map({'male': 0, 'female': 1})
-# Define features and target
-X = df[['Pclass', 'Sex', 'Age', 'Fare']]
+#Load the dataset
+df =pd.read_csv("https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv")
+#Simple preprocessing
+df = df[["Sex","Fare","Survived"]].dropna()
+df['Sex'] = df["Sex"].map({'male':0,'female':1})
+X = df[['Sex','Fare']]
 y = df['Survived']
-# Train the model
-
+#Train the model and save
 model = LogisticRegression()
-model.fit(X, y)
-# Save model
-joblib.dump(model, 'titanic_model.pkl')
+model.fit(X,y)
+joblib.dump(model,'titanic_model.pkl')
